15adf25a6853e8cfa296bc6f070963aefffb7388
nyel-101 <dandevjr@gmail.com>
2025-02-12 08:15:41 +0000

    style(ui): optimize auth tokens

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f0d7b5889a7a48719aa33e5ebd353d8a = () => {
-  return '2026-04-22T13:02:17Z';
+const node_fcee6d7de0764c2b81422ae07036b9bb = () => {
+  return '2025-02-12T08:15:41Z';
 };
