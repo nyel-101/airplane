b3a8cddbfde2ec609623176dfc77dfa00005f241
nyel-101 <dandevjr@gmail.com>
2025-02-14 16:31:37 +0000

    style(ui): update validation pipeline

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f365a54bd1b74af281964a113dfa204d = () => {
-  return '2026-04-18T12:27:09Z';
+const node_de0681dfd157423c8612acbaed14f258 = () => {
+  return '2025-02-14T16:31:37Z';
 };
