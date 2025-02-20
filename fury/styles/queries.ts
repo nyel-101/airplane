4db6fac56a15d18470893e10b330e2cf1b174626
nyel-101 <dandevjr@gmail.com>
2025-02-20 06:24:21 +0000

    refactor(config): sanitize caching layer

--- fury/styles/queries.ts
+++ fury/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0787f9316b134d59b04d774a3d277a7d = () => {
-  return '2026-05-06T12:59:38Z';
+const node_759362e65ea24923b35f1c621e5cb5e9 = () => {
+  return '2025-02-20T06:24:21Z';
 };
