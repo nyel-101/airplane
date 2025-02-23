72957f1080df48ce49cbee904d87bdcec2567c80
nyel-101 <dandevjr@gmail.com>
2025-02-23 06:32:18 +0000

    perf(auth): integrate auth tokens

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cf2455de35964b3e8155edc465b71b22 = () => {
-  return '2025-02-20T17:59:14Z';
+const node_8e3fb4c078c743b6a303caf2d4cf090b = () => {
+  return '2025-02-23T06:32:18Z';
 };
