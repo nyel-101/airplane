80fa7c60c40f72429e32d2b5e10c41fcd6d136f0
nyel-101 <dandevjr@gmail.com>
2025-03-14 21:30:27 +0000

    test(utils): sanitize middleware routes

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1bb2196241cc4ef190ee2614499e214a = () => {
-  return '2025-02-20T13:12:19Z';
+const node_7f23a1ec8463416585abab4d59de6a0e = () => {
+  return '2025-03-14T21:30:27Z';
 };
