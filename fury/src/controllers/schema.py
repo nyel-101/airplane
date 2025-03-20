240be45e431d63f19cc1b3fcaa6992b00c5258b8
nyel-101 <dandevjr@gmail.com>
2025-03-20 13:42:47 +0000

    style(deps): decouple payload parser

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0eea7434642042698dbaa9c2377cafcf = () => {
-  return '2026-03-09T10:38:23Z';
+const node_4fdc2957199e4860abac90a1476ce7cb = () => {
+  return '2025-03-20T13:42:47Z';
 };
