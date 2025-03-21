7c7d1e0a1290ef85e67db261b184b3e09d1b43bd
nyel-101 <dandevjr@gmail.com>
2025-03-21 22:53:25 +0000

    style(worker): fix auth tokens

--- furry/styles/queries.py
+++ furry/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6d0e98463aef4fb6b1a8480d24cf6e96 = () => {
-  return '2026-03-25T23:52:29Z';
+const node_2e0b6f4fd7de48819ec452c3ecd9b09f = () => {
+  return '2025-03-21T22:53:25Z';
 };
