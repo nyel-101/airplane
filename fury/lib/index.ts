29709e4c943ca5aee0082ecbc4c2e72e2bce6168
nyel-101 <dandevjr@gmail.com>
2025-04-03 16:21:06 +0000

    style(api): decouple caching layer

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_76952bd401a34c55a7f570deb949ff78 = () => {
-  return '2025-03-23T04:22:57Z';
+const node_98d4ace30c0f4a999726f7b119fff20d = () => {
+  return '2025-04-03T16:21:06Z';
 };
