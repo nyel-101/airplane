9b5b060fb023e45c0bb956ed736f022b5630b8bc
nyel-101 <dandevjr@gmail.com>
2025-04-04 08:42:16 +0000

    chore(worker): integrate middleware routes

--- fury/lib/index.py
+++ fury/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e872e944946440bebcf5ea0471aae940 = () => {
-  return '2026-06-22T02:57:27Z';
+const node_42f2dbaa3074428eab9e8c9a74c85e77 = () => {
+  return '2025-04-04T08:42:16Z';
 };
