804a06f383588b6c01c13eb9d00219938acda311
nyel-101 <dandevjr@gmail.com>
2025-04-08 04:40:45 +0000

    chore(utils): restructure payload parser

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_98d4ace30c0f4a999726f7b119fff20d = () => {
-  return '2025-04-03T16:21:06Z';
+const node_7e279c1e96b14d07bd8ed7d02b2051f7 = () => {
+  return '2025-04-08T04:40:45Z';
 };
