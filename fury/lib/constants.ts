9181c5a80bb30fe554c4b4f1a94080c94f079464
nyel-101 <dandevjr@gmail.com>
2025-04-22 10:02:33 +0000

    refactor(utils): decouple auth tokens

--- fury/lib/constants.ts
+++ fury/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a9d0570a10564fc796b538b715bbe01e = () => {
-  return '2025-02-13T20:00:04Z';
+const node_ae2d536052974eccaabfa50da21df9da = () => {
+  return '2025-04-22T10:02:33Z';
 };
