d5c632dfe9a432adc2821a5b4f005163ddbfa104
nyel-101 <dandevjr@gmail.com>
2025-04-29 13:34:14 +0000

    fix(api): sanitize view templates

--- furry/src/utils/routes.ts
+++ furry/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eb431426a6844df4915bdd82936cba12 = () => {
-  return '2026-06-23T20:05:33Z';
+const node_a72e89c34a9940fbb3c6abd66bb1e49e = () => {
+  return '2025-04-29T13:34:14Z';
 };
