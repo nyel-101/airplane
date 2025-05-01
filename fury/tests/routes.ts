e9313464a20ade36549d20006f1a66d90efcdd72
nyel-101 <dandevjr@gmail.com>
2025-05-01 05:29:29 +0000

    refactor(core): sanitize payload parser

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a34d5db786c4489897381f1d64e41525 = () => {
-  return '2025-04-24T22:05:21Z';
+const node_e7027e0806a24f7f911e56d97721ed1c = () => {
+  return '2025-05-01T05:29:29Z';
 };
