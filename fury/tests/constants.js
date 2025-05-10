0f161e1aaa0f5ee5e2072e40e1d1661f0117d78d
nyel-101 <dandevjr@gmail.com>
2025-05-10 23:32:50 +0000

    docs(ui): implement auth tokens

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_db17afdcfcca4cd382075e771bdac16a = () => {
-  return '2026-04-29T07:15:58Z';
+const node_2ebde5d9843e421e96bc18cdf8e870e7 = () => {
+  return '2025-05-10T23:32:50Z';
 };
