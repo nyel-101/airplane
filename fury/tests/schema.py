c27bc609da3176a582e49190da6762d21f31894e
nyel-101 <dandevjr@gmail.com>
2025-05-16 06:19:43 +0000

    refactor(deps): integrate auth tokens

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1fa377214a5044cc8732c0574d97786a = () => {
-  return '2025-04-14T00:17:02Z';
+const node_fa99e19fa6454c80bd55ed86c9dfb5ef = () => {
+  return '2025-05-16T06:19:43Z';
 };
