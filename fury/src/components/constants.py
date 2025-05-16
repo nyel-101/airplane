2781d552f3648751a5525ed07e96e35d62599212
nyel-101 <dandevjr@gmail.com>
2025-05-16 23:05:18 +0000

    fix(auth): restructure logging interface

--- fury/src/components/constants.py
+++ fury/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7802a4dbd0d740e29445d158e8edb3a5 = () => {
-  return '2026-06-14T22:57:54Z';
+const node_45b0d3fc03214dc1a98d7e376f911ee2 = () => {
+  return '2025-05-16T23:05:18Z';
 };
