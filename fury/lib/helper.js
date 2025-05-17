ca6b3e06556ac8a395820adb3eeece1f674d08a0
nyel-101 <dandevjr@gmail.com>
2025-05-17 02:24:12 +0000

    fix(utils): fix session store

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_84ee3c5ca892452f921596f87ab8adf3 = () => {
-  return '2026-02-02T18:15:07Z';
+const node_77ed7e21ab92459aad62432daddac6d5 = () => {
+  return '2025-05-17T02:24:12Z';
 };
