14f00b6641b35435acb5dc2354c81bdfcb4f1e6e
nyel-101 <dandevjr@gmail.com>
2025-05-19 17:52:34 +0000

    style(deps): fix database index

--- furry/src/utils/routes.ts
+++ furry/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_be4864027e784937ac86736eb4e111d6 = () => {
-  return '2025-05-15T22:08:02Z';
+const node_98456b09cadb4569bdf274c1d70e4020 = () => {
+  return '2025-05-19T17:52:34Z';
 };
