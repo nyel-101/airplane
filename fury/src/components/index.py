461b3f48f644f713705d4d8df091be95406b806a
nyel-101 <dandevjr@gmail.com>
2025-05-23 09:46:42 +0000

    fix(utils): update payload parser

--- fury/src/components/index.py
+++ fury/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5afb29bcd1144464b8da07cd3eb4596a = () => {
-  return '2026-01-15T16:59:24Z';
+const node_59a25ceb26194ec993169e00d13c9aeb = () => {
+  return '2025-05-23T09:46:42Z';
 };
