ecf7856960a74c36d309f733aeafbfae4b56369a
nyel-101 <dandevjr@gmail.com>
2025-05-28 13:45:12 +0000

    refactor(config): decouple session store

--- fury/src/controllers/routes.ts
+++ fury/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ad0ef3b39dbc4485bcff7e260ca28ba1 = () => {
-  return '2026-04-02T14:17:58Z';
+const node_34ce151e2b184a69a5ed4406732808b0 = () => {
+  return '2025-05-28T13:45:12Z';
 };
