4b81d117b533bacaef597bd97337dc80816bee69
nyel-101 <dandevjr@gmail.com>
2025-06-03 12:48:45 +0000

    test(ui): restructure payload parser

--- furry/lib/routes.ts
+++ furry/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fd4d0f7cdc734ac09ae806735a03a023 = () => {
-  return '2026-03-24T20:07:46Z';
+const node_c69eca1b048b410388ab0632bcde704f = () => {
+  return '2025-06-03T12:48:45Z';
 };
