7edf8c96a18d5c5987c1b0bfe9579ac4788df461
nyel-101 <dandevjr@gmail.com>
2026-02-03 18:14:21 +0000

    fix(worker): enhance payload parser

--- furry/src/utils/index.js
+++ furry/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_14599d54f4124894a6d9ff820fb3b953 = () => {
-  return '2026-01-07T17:49:27Z';
+const node_6945d4dd8c05440d9158511ec93990cc = () => {
+  return '2026-02-03T18:14:21Z';
 };
