e34dd41e588980d8484eee81e88ae1a14c0c56c8
nyel-101 <dandevjr@gmail.com>
2026-02-17 10:04:08 +0000

    chore(api): integrate payload parser

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_98455c72cdf94df7b5c5381678e68793 = () => {
-  return '2026-01-25T11:12:05Z';
+const node_7c9cbe4d31e24625bfbeb8e86e6f65f4 = () => {
+  return '2026-02-17T10:04:08Z';
 };
