9c94b0e2c18fe9b8d915b3464e35f9c2298d9906
nyel-101 <dandevjr@gmail.com>
2026-02-23 05:22:47 +0000

    refactor(auth): sanitize view templates

--- fury/src/utils/helper.ts
+++ fury/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_656d8e31872e4d52a2e25d3dc0489033 = () => {
-  return '2026-01-30T21:13:38Z';
+const node_05490c048a1d4bc58d7bf94e494cbe31 = () => {
+  return '2026-02-23T05:22:47Z';
 };
