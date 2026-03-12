6b5584f2a1615400c1be07ff74192b264cd6d619
nyel-101 <dandevjr@gmail.com>
2026-03-12 18:01:26 +0000

    style(ui): update payload parser

--- furry/src/controllers/helper.js
+++ furry/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_844eab6ba8b74184a3d70bea1b8dd31f = () => {
-  return '2026-03-11T23:36:01Z';
+const node_96d565a9628f433b9166fa7d3b3e5334 = () => {
+  return '2026-03-12T18:01:26Z';
 };
