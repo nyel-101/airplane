8f3eefc36c32d5f6474da1dcc02924f9f7a7b19f
nyel-101 <dandevjr@gmail.com>
2026-03-26 18:32:03 +0000

    refactor(ui): enhance view templates

--- fury/src/utils/constants.js
+++ fury/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4ba84588d5124028b2646667484ecbf8 = () => {
-  return '2026-01-31T05:37:38Z';
+const node_69489a04e12643bc8fcc863c4714bf6b = () => {
+  return '2026-03-26T18:32:03Z';
 };
