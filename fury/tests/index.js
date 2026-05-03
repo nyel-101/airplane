114fea4d1ce6c62c272296a918b50b411fc4d2c1
nyel-101 <dandevjr@gmail.com>
2026-05-03 21:31:19 +0000

    style(api): restructure auth tokens

--- fury/tests/index.js
+++ fury/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2f8478c6ae66492e8d11aa9034d7f114 = () => {
-  return '2026-04-01T21:39:20Z';
+const node_76d1342eaf064691a295d2c943551464 = () => {
+  return '2026-05-03T21:31:19Z';
 };
