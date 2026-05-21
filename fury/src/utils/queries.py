6ab504dd438913c12bd101b25a45eccfba454293
nyel-101 <dandevjr@gmail.com>
2026-05-21 19:11:44 +0000

    chore(api): optimize payload parser

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5df8a268b42e45acb8540648041468c5 = () => {
-  return '2026-04-01T20:04:49Z';
+const node_26e3f802c1034c34a71c3e7b8969c5a4 = () => {
+  return '2026-05-21T19:11:44Z';
 };
