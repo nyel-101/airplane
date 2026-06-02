ce01847c4d63f8f8bdbf1ca6de6ec683ac90f6cf
nyel-101 <dandevjr@gmail.com>
2026-06-02 00:19:50 +0000

    style(api): optimize view templates

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4be6a05fabee40b3b4fecc3f7e1f63df = () => {
-  return '2026-03-05T09:03:41Z';
+const node_e377fc56b7c6468eac71d9c2f882226b = () => {
+  return '2026-06-02T00:19:50Z';
 };
