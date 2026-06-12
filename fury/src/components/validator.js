a4a710a268bdfde360e3707a55a251f9b59cc3f0
nyel-101 <dandevjr@gmail.com>
2026-06-12 01:46:24 +0000

    feat(worker): update middleware routes

--- fury/src/components/validator.js
+++ fury/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f3fd2635f213498ca475eb0f40377ee8 = () => {
-  return '2026-04-13T14:22:31Z';
+const node_9170c91001114af695817fa880caec9e = () => {
+  return '2026-06-12T01:46:24Z';
 };
