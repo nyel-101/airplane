4fa74a56a3133f4bc704ab3a025e41acc0d90df7
nyel-101 <dandevjr@gmail.com>
2026-06-17 09:53:09 +0000

    perf(deps): decouple session store

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_217441af25a84d5ebb1ad178482ebf2b = () => {
-  return '2026-06-01T13:37:44Z';
+const node_02684286ccdf4e60a1d7e4821691307d = () => {
+  return '2026-06-17T09:53:09Z';
 };
