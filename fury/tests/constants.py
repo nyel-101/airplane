b677d3480e2e796b1f03af86c89040a537c38ed1
nyel-101 <dandevjr@gmail.com>
2026-06-19 10:38:45 +0000

    chore(utils): optimize payload parser

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_30e1486dfa194eb39545ab5ffe69f812 = () => {
-  return '2026-02-17T07:16:41Z';
+const node_5782e3671f5e4b8bb11ff77ea7edce36 = () => {
+  return '2026-06-19T10:38:45Z';
 };
